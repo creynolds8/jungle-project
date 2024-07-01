92994e8ed17bf22f271f0790abf376f1dee9178d
creynolds8 <cameron8.cr@gmail.com>
2024-07-01 14:43:18 -0700

    add user authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
 
+  def self.authenticate_with_credentials email, password
+    trimmed_email = email.strip.downcase
+    user = User.find_by(email: trimmed_email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { in: 8..20 }
