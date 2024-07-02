79a1e823b9e7d1ccf3d8dfbcd296981d379279c0
creynolds8 <cameron8.cr@gmail.com>
2024-07-02 14:36:16 -0700

    auto format file

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -1,14 +1,14 @@
 describe("Product details", () => {
   it("visits the product details page for a particular product", () => {
-    cy.visit("/")
+    cy.visit("/");
     cy.get(".products article").should("have.length", 2);
-    cy.get(".products article").first().click()
-    cy.get(".quantity span").contains("in stock at")
-  })
+    cy.get(".products article").first().click();
+    cy.get(".quantity span").contains("in stock at");
+  });
   it("visits the product details page for a different product", () => {
-    cy.visit("/")
+    cy.visit("/");
     cy.get(".products article").should("have.length", 2);
-    cy.get(".products article").last().click()
-    cy.get(".quantity span").contains("in stock at")
-  })
-})
+    cy.get(".products article").last().click();
+    cy.get(".quantity span").contains("in stock at");
+  });
+});
