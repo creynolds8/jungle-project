48383c65bdee7973456dc1e5920541d4a79e8910
creynolds8 <cameron8.cr@gmail.com>
2024-07-02 13:41:44 -0700

    add second test to double check correct linking to product show page

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -5,4 +5,10 @@ describe("Product details", () => {
     cy.get(".products article").first().click()
     cy.get(".quantity span").contains("in stock at")
   })
+  it("visits the product details page for a different product", () => {
+    cy.visit("/")
+    cy.get(".products article").should("have.length", 2);
+    cy.get(".products article").last().click()
+    cy.get(".quantity span").contains("in stock at")
+  })
 })
