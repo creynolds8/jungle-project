476c7b103232bb80b4f2d7a99f1c2aac862babf0
creynolds8 <cameron8.cr@gmail.com>
2024-07-02 14:37:39 -0700

    add test for adding product from product page

--- cypress/e2e/add_to_cart.cy.js
+++ cypress/e2e/add_to_cart.cy.js
@@ -1,10 +1,18 @@
 describe("Add to Cart", () => {
-  it("There is products on the page", () => {
-    cy.visit("/")
-    cy.contains("Welcome to The Jungle")
+  it("should add the product to the cart and increase the cart total", () => {
+    cy.visit("/");
+    cy.contains("Welcome to The Jungle");
     cy.get(".products article").should("have.length", 2);
-    cy.get(".products article").first()
-    cy.get("button").contains("Add").click()
-    cy.contains("My Cart (1)")
+    cy.get(".products article").first();
+    cy.get("button").contains("Add").click();
+    cy.contains("My Cart (1)");
   });
-})
+  it("should add a product to the cart from the product page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
+    cy.get(".products article").first().click();
+    cy.get(".quantity span").contains("in stock at");
+    cy.get("button").contains("Add").click();
+    cy.contains("My Cart (1)");
+  });
+});
