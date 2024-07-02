263c583b797bcb13fb2d161c2ab08eb48f5f22e8
creynolds8 <cameron8.cr@gmail.com>
2024-07-02 13:40:06 -0700

    add test to check that user can navigate to product show pages

---
+++ cypress/e2e/product_details.cy.js
@@ -0,0 +1,8 @@
+describe("Product details", () => {
+  it("visits the product details page for a particular product", () => {
+    cy.visit("/")
+    cy.get(".products article").should("have.length", 2);
+    cy.get(".products article").first().click()
+    cy.get(".quantity span").contains("in stock at")
+  })
+})
