ceb91fa261025372e132e7c74cf2543ee786c844
creynolds8 <cameron8.cr@gmail.com>
2024-07-03 13:47:14 -0700

    create form with for submitting new review - working, delete functionality - not working backend

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+    
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to @product
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to @product
+  end
+
+  private 
+
+  def review_params
+    params.require(:review).permit(:rating, :description)
+  end
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :user_id, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, inclusion: {in: 1..5}
 end
